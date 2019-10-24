d8a73cf4fa3e2231f19ecfd1a697c367d0ee3549
societynong <172065208@qq.com>
2019-10-24 12:39:50 +0800

    时间长度与性能的分析

--- main.py
+++ main.py
@@ -4,25 +4,19 @@ import numpy as np
 import matplotlib.pyplot as plt
 import time
 
-def getParameter():
-    global FS,DUR,t,SNR,WIN,STRD,L,N_USED,f0,NFFT
+FS = 10000
+DUR = 60 * 1
 
-    FS = 10000
-    DUR = 60 * 20
+SNR = -20
+WIN = 10000
+STRD = 10000
+N_USED = DUR * FS
+f0 = 100
+NFFT = 12000000
 
-    SNR = -40
-    WIN = 12000000
-    STRD = 12000000
-    nOverlap = np.ceil(WIN * (WIN - STRD) / WIN)
-    L = int(np.floor((FS*DUR - nOverlap) / (WIN - nOverlap)))
-    N_USED = int((WIN - nOverlap) * (L - 1) + WIN)
-    f0 = 100
-    NFFT = 12000000
-    t = np.arange(N_USED) / FS
 
 def getSig():
-
-
+    t = np.arange(N_USED) / FS
     x0 = np.sin(2*np.pi*f0*t)
     ns = np.random.randn(N_USED)
     ns = funs.butter_lowpass_filter(ns,10,300,FS)
@@ -38,7 +32,7 @@ def getSig():
 
 
 
-getParameter()
+
 
 
 def testNormal():
@@ -46,13 +40,15 @@ def testNormal():
     x0, ns0 = getSig()
     sig = x0 + ns0
     # F = funs.accumlateInF(sig, FS, NFFT, WIN, STRD, L).__abs__()
-    F = np.abs(np.fft.fft(sig,NFFT))
+    F = np.abs(np.fft.fft(sig,NFFT))  / len(sig)
     end = time.time()
     # print(end - start)
     # plt.figure()
     fP = f0 * NFFT // FS
-    plt.plot(abs(F[:300 * NFFT // FS]))
-    plt.title(F[fP] / np.mean(np.hstack((F[0:fP - 1], F[fP + 1:300 * NFFT // FS]))))
+    f = np.arange(300 * NFFT // FS) / (300 * NFFT // FS) * 300
+    plt.plot(f,abs(F[:300 * NFFT // FS]))
+    plt.xlabel("Hz")
+    plt.title("Mean of Noise:{:.3f},Mean of Signal{:.3f},Time:{}min(s)".format(np.mean(np.hstack((F[0:fP - 1], F[fP + 1:300 * NFFT // FS]))),F[fP],DUR / 60))
     plt.show()
     # f = np.arange(funs.NFFT) * funs.FS / funs.NFFT
     # F = abs(funs.accumlateInF(sig,funs.FS,funs.NFFT,funs.WIN,funs.STRD,funs.L))
@@ -77,6 +73,35 @@ def testNoise():
     #     sigAcum += sig[i:i+w]
     # sigAcum /= len(range(0,len(sig) - w,s))
     # print(np.mean(sigAcum.__abs__()))
+def testTandSNR(TRef,SNRRef,fd):
+    global DUR,SNR,N_USED
+    DUR = 60 * TRef
+    SNR = SNRRef
+    N_USED = DUR * FS
+    x0, ns0 = getSig()
+    sig = x0 + ns0
+    # F = funs.accumlateInF(sig, FS, NFFT, WIN, STRD, L).__abs__()
+    F = np.abs(np.fft.fft(sig,NFFT))  / len(sig)
+    fP = f0 * NFFT // FS
+    f = np.arange(300 * NFFT // FS) / (300 * NFFT // FS) * 300
+    plt.plot(f,abs(F[:300 * NFFT // FS]))
+    plt.xlabel("Hz")
+    plt.title("Mean of Noise:{:.3f},Mean of Signal{:.3f},Time:{}min(s)".format(np.mean(np.hstack((F[0:fP - 1], F[fP + 1:300 * NFFT // FS]))),F[fP],TRef))
+    # plt.show()
+    plt.savefig("TandSNR/test_{}_{}.png".format(TRef,SNRRef))
+    plt.close()
+
+    fd.write("{}\t{}\t{}\t{}\n".format(TRef,SNRRef,np.mean(np.hstack((F[0:fP - 1], F[fP + 1:300 * NFFT // FS]))),F[fP]))
+
 
 if __name__ == '__main__':
-    testNormal()
+    Ts = [1,2,4,8,16,32,64]
+    SNRs = [-10,-20,-30,-40,-50]
+    with open("TandSNR/ExploreTandSNR.txt","a+") as fd:
+        cnt = 0
+        for snr in SNRs:
+            testTandSNR(8,snr,fd)
+            cnt += 1
+            print("{} ok!".format(cnt / len(SNRs)))
+
+
